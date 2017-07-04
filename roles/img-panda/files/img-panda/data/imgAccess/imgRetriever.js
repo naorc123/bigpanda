04cb77f919d8cc53c5447a81c90b49248eef3199
naorc123 <naorc123@gmail.com>
2017-07-04 20:31:54 +0300

    Update imgRetriever.js

--- roles/img-panda/files/img-panda/data/imgAccess/imgRetriever.js
+++ roles/img-panda/files/img-panda/data/imgAccess/imgRetriever.js
@@ -1,7 +1,7 @@
 // function that read the images dir and return an arr of the files`
 var fs = require('fs');
 function GetImage() {
-        return fs.readdirSync('/root/resources'); // Img folder here.
+        return fs.readdirSync('/tmp/resources'); // Img folder here.
 }
 
 module.exports={
