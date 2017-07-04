6a0c53ef946b4c4ceac6bd96525fd1d6a9e0c3d7
naorc123 <naorc123@gmail.com>
2017-07-04 20:27:26 +0300

    Create ExtractFilePath.js

---
+++ roles/img-panda/files/img-panda/util/ExtractFilePath.js
@@ -0,0 +1,15 @@
+// function that get a arr of the dir's files calc the lengh of the arr and random a number between this lengh
+//  In the end the function return a random path compare to the random num that she get.
+function ExtractFilePath(files){
+           var imgAr = files;
+           var path = '/root/resources/'; //  images dir
+           var num = Math.floor( Math.random() * imgAr.length );
+           var img = imgAr[ num ];
+           var img_path = path + img;
+           return (img_path);
+}
+
+// The module.exports suppesed to aloow me to get acces to this function from another file.
+module.exports = {
+        ExtractFilePath:ExtractFilePath
+}
